68927246d314ec6c692edcd871a453a7e01ec380
canoe-soda <71360416+canoe-soda@users.noreply.github.com>
2020-11-22 17:12:11 +0700

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("Mother Dragon", "MONDRAG", 18) {
+    constructor () public ERC20Detailed("Mother Dragon", "MODRAG", 18) {
         _mint(msg.sender, 125000 * (10 ** uint256(decimals())));
     }
 }
