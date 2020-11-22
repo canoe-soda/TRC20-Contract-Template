9f3db268ddc482ce2f42e542416a2c01a2d9a33d
canoe-soda <71360416+canoe-soda@users.noreply.github.com>
2020-11-22 15:32:17 +0700

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is ERC20, ERC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("Drug", "DRUG", 18) {
-        _mint(msg.sender, 500000 * (10 ** uint256(decimals())));
+    constructor () public ERC20Detailed("Tuna", "TUNA", 18) {
+        _mint(msg.sender, 30000 * (10 ** uint256(decimals())));
     }
 }
